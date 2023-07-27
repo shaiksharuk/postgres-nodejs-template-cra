fdbaf26706329a7790dedd2865e69bf2a0249015
shaiksharuk <shaiksharuk998@gmail.com>
2023-07-27 15:26:08 +0530

    implement add and edit message functions

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,24 @@ function App() {
   const addItem = (item: Item) => {
     setItems([...items, item]);
   }
+  const editItem = (item: Item) => {
+    const idx = items.findIndex((i) => i.id===item.id)
+    if (idx < 0) {
+      window.alert(`invlaid idx: ${idx}`);
+      return
+    }
+    const newItems = [...items];
+    newItems[idx] = {
+      id: item.id,
+      message: item.message,
+    }
+    setItems(newItems);
+  }
   return (
     <div className="App">
       <h1>Sharuk's App</h1>
       <ItemInput addItem={addItem} maxId={Math.max(...[0, ...items.map(i => i.id)])} />
-      <ItemList items={items} />
+      <ItemList items={items} editItem={editItem}/>
     </div>
   );
 }
--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -28,24 +28,55 @@ export type Item = {
   message: string;
 }
 
-type ItemProps = Item;
+type ItemViewProps = {
+  item: Item;
+  editItem: (item: Item) => void;
+};
 
-function ItemView({ id, message }: ItemProps) {
-  return (
+function ItemView({ item: { id, message }, editItem }: ItemViewProps) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [text, setText] = useState(message);
+  const startEditing = () => {
+    setIsEditing(true);
+  }
+  const cancelEditing = () => {
+    setIsEditing(false);
+    setText(message);
+  }
+  const textChangeHandler: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setText(event.target.value)
+  }
+  const editItemHandler = () => {
+    editItem({ id, message: text });
+    setIsEditing(false);
+  }
+  const whenViewing = (
     <div>
       id: {id}
       <br />
       message: {message}
+      <button onClick={startEditing}>edit</button>
     </div>
   )
+  const whenEditing = (
+    <div>
+      id: {id}
+      <br />
+      <input type="text" value={text} onChange={textChangeHandler} />
+      <button onClick={editItemHandler}>apply</button>
+      <button onClick={cancelEditing}>cancel</button>
+    </div>
+  )
+  return isEditing ? whenEditing : whenViewing;
 }
 
 type ItemListProps = {
   items: Item[]
+  editItem: (item: Item) => void
 }
 
 export function ItemList(props: ItemListProps) {
-  const renderedItems = props.items.map(i => <ItemView {...i} />)
+  const renderedItems = props.items.map(i => <ItemView item={i} editItem={props.editItem}/>)
   return (
     <div>
       {renderedItems}
